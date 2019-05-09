2e825f607f6d36afcb3ec446b3d70f2117a18bd1
MrGM <chminggao@163.com>
2019-05-09 08:24:30 +0800

    first

---
+++ NetCore/Data/NetCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NetCore.Movies;
+
+namespace NetCore.Models
+{
+    public class NetCoreContext : DbContext
+    {
+        public NetCoreContext (DbContextOptions<NetCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NetCore.Movies.Movie> Movie { get; set; }
+    }
+}
---
+++ NetCore/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NetCore.Models;
+using NetCore.Movies;
+
+namespace NetCore.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly NetCore.Models.NetCoreContext _context;
+
+        public IndexModel(NetCore.Models.NetCoreContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
